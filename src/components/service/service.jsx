907b66e81b7e702ca5804b194eb9e12fe08e569d
purvanshu <purukhapre12@gmail.com>
2025-09-22 17:10:49 +0530

    second commit

--- src/components/service/service.jsx
+++ src/components/service/service.jsx
@@ -10,7 +10,7 @@ const Service = () => {
 
       <div className="steps-cards">
         <div className="step-card">
-          <img src="public/car_1634207.png"/>
+          <img src="/car_1634207.png"/>
           <h3>Select Car</h3>
           <p>
             We offer a wide range of vehicles to meet your needs. Whether you're commuting or heading out of town, we've got the right car for you.
@@ -18,7 +18,7 @@ const Service = () => {
         </div>
 
         <div className="step-card">
-          <img src="public/operator_7438023.png"/>
+          <img src="/operator_7438023.png"/>
           <h3>Contact Operator</h3>
           <p>
             Friendly and knowledgeable staff are here to help you with questions, bookings, or anything you need before hitting the road.
@@ -26,7 +26,7 @@ const Service = () => {
         </div>
 
         <div className="step-card">
-          <img src="public/test-drive_9549460.png"/>
+          <img src="/test-drive_9549460.png"/>
           <h3>Let’s Drive</h3>
           <p>
             Just grab the keys and go! Enjoy a smooth, flexible ride with full support from pick-up to drop-off.
